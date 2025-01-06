530b829e77ac0966410b529acc6d0806627c0236
Fin Watling <finlaywatling@gmail.com>
2025-01-06 01:00:07 +0000

    Started leetcode integration

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-const img = document.getElementById('bounce');
 
-let x = 100;
-let y = 100;
-let speedX = 2;
-let speedY = 2;
+async function getLeetcode() {
+    try {
 
-const maxWidth = window.innerWidth - img.width;
-const maxHeight = window.innerHeight - img.height;
+        const response = await fetch("https://leetcode-stats-api.herokuapp.com/finwatling");
 
-function bounceImage() {
-    x += speedX;
-    y += speedY;
+        const data = await response.json();
 
-    if (x <= 0 || x >= maxWidth) {
-        speedX = -speedX;
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching Leetcode data:', error);
     }
-
-    if (y <= 0 || y >= maxHeight) {
-        speedY = -speedY;
-    }
-
-    img.style.left = x + 'px';
-    img.style.top = y + 'px';
-    
-    requestAnimationFrame(bounceImage);
 }
 
-bounceImage();
+getLeetcode();
+
