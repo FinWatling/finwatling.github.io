832e9649d18a8432bb2e12bbfac3d5e1820677c1
Fin Watling <finlaywatling@gmail.com>
2025-01-06 01:11:21 +0000

    Added Leetcode integration.. Need to style :S

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-
-async function getLeetcode() {
+async function getLeetcodeData() {
     try {
-
         const response = await fetch("https://leetcode-stats-api.herokuapp.com/finwatling");
-
         const data = await response.json();
 
-
-        return data;
+        if (data.status === "error") {
+            document.getElementById("error-message").innerText = data.message;
+        } else {
+            document.getElementById("totalSolved").innerText = data.totalSolved;
+            document.getElementById("acceptanceRate").innerText = `${data.acceptanceRate}%`;
+            document.getElementById("easySolved").innerText = data.easySolved;
+            document.getElementById("mediumSolved").innerText = data.mediumSolved;
+            document.getElementById("hardSolved").innerText = data.hardSolved;
+        }
     } catch (error) {
         console.error('Error fetching Leetcode data:', error);
+        document.getElementById("error-message").innerText = "Failed to load data.";
     }
 }
 
-getLeetcode();
-
+getLeetcodeData();
